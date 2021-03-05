1642c0a9c4e0165e42713e81b960c2778356d0d9
Bilboking <brkbendreality@gmail.com>
2021-03-05 13:38:46 -0600

    updated code

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,14 +9,17 @@ const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
     <h1>React.js Project Assignment App for Kingsland University</h1>
-    <p>Public and private aspects of app included.</p>
-    <p>Now go build something great.</p>
+    <p><b>Requirements</b></p>
+    <p>Public and private parts with authentication.</p>
+    <p>At least 3 different dynamic pages with DB and standard React stuff.</p>
+    
+    <p><b>Bruce writing code for this assignment.</b></p>
     <StaticImage
-      src="../images/gatsby-astronaut.png"
+      src="../images/Bruce-writing-code-pic.png"
       width={300}
       quality={95}
       formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
+      alt="Bruce King writing code"
       style={{ marginBottom: `1.45rem` }}
     />
     <p>
