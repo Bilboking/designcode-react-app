f0cb0a5129795ff4e4479d04b859212e4d8b3f70
Bilboking <brkbendreality@gmail.com>
2021-03-05 16:10:10 -0600

    top section of first page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,12 +8,15 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>React.js Project Assignment App for Kingsland University</h1>
-    <p><b>Requirements</b></p>
-    <p>Public and private parts with authentication.</p>
-    <p>At least 3 different dynamic pages with DB and standard React stuff.</p>
-    
-    <p><b>Bruce writing code for this assignment.</b></p>
+    <div className="Hero">
+      <div className="HeroGroup">
+          <h1>React.js Project Assignment App for Kingsland University</h1>
+          <p><b>Requirements</b></p>
+          <p>Public and private parts with authentication.</p>
+          <p>At least 3 different dynamic pages with DB and standard React stuff.</p>
+          <p>This page is built using the Gatsby template as oppossed to the 'create-react-app' template. This allows Router for multiple pages, GraphQL for database, Helmet for SEO, etc. to be templated
+          right away to save time and expense.</p>
+           <p><b>Bruce writing code for this assignment.</b></p>
     <StaticImage
       src="../images/Bruce-writing-code-pic.png"
       width={300}
@@ -22,10 +25,9 @@ const IndexPage = () => (
       alt="Bruce King writing code"
       style={{ marginBottom: `1.45rem` }}
     />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+<Link to="/page-2/">Click here for page 2</Link> 
+      </div>
+    </div>   
   </Layout>
 )
 
