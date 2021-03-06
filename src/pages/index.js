f4647e194903c6c91520cd399a992d3b18b5e6fe
Bilboking <brkbendreality@gmail.com>
2021-03-05 20:30:04 -0600

    working on header

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,11 +12,11 @@ const IndexPage = () => (
       <div className="HeroGroup">
           <h1>React.js Project Assignment App for Kingsland University</h1>
           <p><b>Requirements</b></p>
-          <p>Public and private parts with authentication.</p>
+          <p>A SPA with a public area and private area with authentication. I am choosing Auth0 token based authentication with the option for multi-factor protection since web security breaches (ransom ware, spy/mal ware, etc.) are common and can be very damaging to users. Klaus Schwab of the World Economic Forum states that the next pandemic will be a cyberwar(already happening, SolarWinds etc.) </p>
           <p>At least 3 different dynamic pages with DB and standard React stuff.</p>
           <p>This page is built using the Gatsby template as oppossed to the 'create-react-app' template. This allows Router for multiple pages, GraphQL for database, Helmet for SEO, etc. to be templated
           right away to save time and expense.</p>
-          <Link to="/page-2/">Click here for page 2</Link> 
+          <Link to="/page-2/">Watch video demo of project</Link> 
            <p><b>Bruce writing code for this assignment.</b></p>
     
     <StaticImage
