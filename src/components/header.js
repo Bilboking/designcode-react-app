4c6662d240b2ae1ab7d09beda8d66e5eaafc1b29
Bilboking <brkbendreality@gmail.com>
2021-03-06 11:07:09 -0600

    header work

--- src/components/header.js
+++ src/components/header.js
@@ -1,12 +1,17 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
+import './Header.css'
+import logo from '../images/small-react-logo.png'
 
 const Header = ({ siteTitle }) => (
   <div className="Header">
     <div className="HeaderGroup">
-      <Link to="/"><img src="" width="30" /></Link>
-      <Link to="/page-2">Page 2</Link>
+   <Link to="/"><img src={logo} alt="" width="30" /></Link>
+      <Link to="/page-2">Page 2  </Link>
+      <Link to="/page-3">Page 3  </Link>
+      <Link to="/workshops">Workshops </Link>
+      <Link to="/buy"><button>Buy</button></Link>
     </div>
   </div>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const IndexPage = () => (
       <div className="HeroGroup">
           <h1>React.js Project Assignment App for Kingsland University</h1>
           <p><b>Requirements</b></p>
-          <p>A SPA with a public area and private area with authentication. I am choosing Auth0 token based authentication with the option for multi-factor protection since web security breaches (ransom ware, spy/mal ware, etc.) are common and can be very damaging to users. Klaus Schwab of the World Economic Forum states that the next pandemic will be a cyberwar(already happening, SolarWinds etc.) </p>
+          <p>A SPA with a public area and private area with authentication. I am choosing Auth0 token based authentication with the option for multi-factor protection since web security breaches (ransom ware, spy/mal ware, etc.) are common and can be very damaging to users. </p>
           <p>At least 3 different dynamic pages with DB and standard React stuff.</p>
           <p>This page is built using the Gatsby template as oppossed to the 'create-react-app' template. This allows Router for multiple pages, GraphQL for database, Helmet for SEO, etc. to be templated
           right away to save time and expense.</p>
